1acf78c1043a3a2a3200402798028d21a07ef678
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-16 09:29:22 +1100

    Show user profile image in ChatMessage

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,9 +1,13 @@
 const ChatMessage = ({ message, auth }) => {
   const { text, uid } = message;
-  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+  const { currentUser } = auth;
+  const { photoURL } = currentUser;
+
+  const messageClass = uid === currentUser.uid ? "sent" : "received";
 
   return (
     <div className={`message ${messageClass}`}>
+      <img src={photoURL} alt="" height="32" width="32" />
       <p>{text}</p>
     </div>
   );
